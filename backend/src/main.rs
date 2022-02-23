6f1b4184ef9ee7cf6923c717b8bea6e1c5d087d1
Yuki Kodama <endflow.net@gmail.com>
2022-02-23 18:32:14 +0900

    Add backend app

---
+++ backend/src/main.rs
@@ -0,0 +1,26 @@
+use axum::routing::{get, Router};
+use std::net::SocketAddr;
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt::init();
+
+    let app = Router::new()
+        .route("/", get(root))
+        .route("/hello", get(hello));
+
+    let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
+    tracing::debug!("listening on {}", addr);
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn root() -> &'static str {
+    "Hello, World!"
+}
+
+async fn hello() -> &'static str {
+    "World"
+}
