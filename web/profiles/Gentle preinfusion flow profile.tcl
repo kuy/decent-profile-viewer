9f5770ad527017efe5661a4e3958fda6b2f7b8fb
Yuki Kodama <endflow.net@gmail.com>
2022-02-26 13:41:58 +0900

    Add profiles

---
+++ web/profiles/Gentle preinfusion flow profile.tcl
@@ -0,0 +1,31 @@
+advanced_shot {}
+author Decent
+espresso_decline_time 0
+espresso_hold_time 21
+espresso_pressure 9.0
+espresso_temperature 92.0
+final_desired_shot_volume 37
+final_desired_shot_volume_advanced 135
+final_desired_shot_weight 36
+final_desired_shot_weight_advanced 135
+flow_profile_decline 1.8
+flow_profile_decline_time 23
+flow_profile_hold 2.2
+flow_profile_hold_time 0
+flow_profile_minimum_pressure 6
+flow_profile_preinfusion 4.2
+flow_profile_preinfusion_time 6
+preinfusion_flow_rate 3.5
+preinfusion_guarantee 0
+preinfusion_stop_pressure 4.0
+preinfusion_time 30
+pressure_end 1.0
+profile_hide 1
+profile_language en
+profile_notes {Created by a famously innovative Seattle-based espresso machine manufacturer, this technique works well with aromatic, lightly roasted coffee beans.}
+profile_title {Gentle preinfusion flow profile}
+settings_profile_type settings_2b
+tank_desired_water_temperature 0
+water_temperature 80
+beverage_type {espresso}
+
---
+++ web/profiles/Gentler but still traditional 8.4 bar.tcl
@@ -0,0 +1,31 @@
+advanced_shot {}
+author Decent
+espresso_decline_time 2
+espresso_hold_time 31
+espresso_pressure 8.4
+espresso_temperature 89.5
+final_desired_shot_volume 37
+final_desired_shot_volume_advanced 135
+final_desired_shot_weight 36
+final_desired_shot_weight_advanced 135
+flow_profile_decline 1
+flow_profile_decline_time 23
+flow_profile_hold 1.8
+flow_profile_hold_time 0
+flow_profile_minimum_pressure 6
+flow_profile_preinfusion 4.2
+flow_profile_preinfusion_time 6
+preinfusion_flow_rate 4
+preinfusion_guarantee 0
+preinfusion_stop_pressure 4.0
+preinfusion_time 10
+pressure_end 6.0
+profile_hide 1
+profile_language en
+profile_notes {Professional baristas worldwide have found that slightly lowering the pressure from 9 bar down to 8.4 produces great espresso more often.  The gentler pressure still produces traditional flavors but requires less perfect preparation skills from the barista.}
+profile_title {Gentler but still traditional 8.4 bar}
+settings_profile_type settings_2a
+tank_desired_water_temperature 0
+water_temperature 86.0
+beverage_type {espresso}
+
